c956b3207229153e766af2ccfd4596dc988669cc
Jean-Luc <jeanlucv@berkeley.edu>
2023-06-28 20:24:21 -0700

    added some questions and license list

--- Spontanous_Readme/index.js
+++ Spontanous_Readme/index.js
@@ -4,10 +4,57 @@ const inquirer = require('inquirer')
 const questions = [
     {
         type: "input",
-        name:"title",
+        name: "title",
         message:"Give a name for your project"
 
-    }
+    },
+    {
+        type: "input",
+        name: "description",
+        message:"Give a description for your project"
+
+    },
+    {
+        type: "input",
+        name: "installation",
+        message:"What steps are required to run this project?"
+
+    },
+    {
+        type: "input",
+        name: "usage",
+        message:"Give a name for your project"
+
+    },
+    {
+        type: "input",
+        name: "installation",
+        message:"Give a name for your project"
+
+    },
+    {
+        type: "input",
+        name: "credit",
+        message:"give a list contributors"
+
+    },
+    {
+        type: "list",
+        name: "license",
+        message:"Select a License",
+        choices: [
+            "MIT",
+            "Morzilla",
+            "Apache 2.0",
+            "GNU General Public license 3.0",
+            "Boost Software 1.0"
+
+
+        ]
+
+        
+
+    },
 
 ];
 
